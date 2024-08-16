f4b860f95bba975540e6f2f589c4d7ef41672dca
Satendra <satendrachauhan7665234@gmail.com>
2024-08-17 00:11:41 +0530

    First message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
-import { Person } from "@mui/icons-material"
+import { Add, Chat, FavoriteRounded, Notifications, Person, QuestionMark } from "@mui/icons-material"
 import MenuContainer from './Component/MenuContainer'
 
 const App = () => {
+
+  useEffect(() => {
+    const allIcon = document.querySelectorAll(".iconContainer")
+    
+
+    function setMenuActive(){
+      allIcon.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    allIcon.forEach(n => n.addEventListener("click" , setMenuActive))
+  } , [])
+
   return (
     <div className="App">
     <div className = "menuContainer">
@@ -12,12 +25,29 @@ const App = () => {
       <div className='subMenu'>
          <div>
             <MenuContainer icon= {<Person />}/>
+            <MenuContainer icon= {<Notifications />}/>
+            <MenuContainer icon= {<Chat />}/>
+         </div>
+         <div>
+            <MenuContainer icon= {<FavoriteRounded />}/>
+         </div>
+         <div>
+            <MenuContainer icon= {<QuestionMark />}/>
+            <MenuContainer icon= {<Add />}/>
          </div>
       </div>  
     </div>
 
 
-    <main className = "main"></main>
+    <main className = "main">
+      <div className='searchBox'>
+        <input type='text' placeholder='Search..' />
+        <div className='search'>
+            <img src='pinarrow.png' alt='' />
+        </div>
+      </div>
+      <div className='mainContainer'></div>
+    </main>
   </div>
   )
 }
